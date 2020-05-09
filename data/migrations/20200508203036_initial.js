f351a68d1b154c00f15ce30bf243d49edcc28ed1
Amber-Pittman <amber@amberpittman.com>
2020-05-08 20:36:52 -0400

    Created users table

---
+++ data/migrations/20200508203036_initial.js
@@ -0,0 +1,13 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable("users", (table) => {
+        table.increments()
+        table.text("username", 128).notNull().unique()
+        table.text("password", 128).notNull()
+    })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("users")
+  
+};
